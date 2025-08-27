b7eb34cf0636d1bd2f0bc9b17d50f271d3e33482
Celesteisscool <azzzlol5227@gmail.com>
2025-08-27 09:20:12 -0400

    add some more emoticons

--- emoticons.js
+++ emoticons.js
@@ -232,5 +232,22 @@ function setupAllEmojis() {
     addEmoticon("ඞා", ["sus"]);
     addEmoticon("𐐘", ["sus"]);
 
-
+    // Other
+    addEmoticon("(¬_¬)", ["dull", "brows"]);
+    addEmoticon("(* ^ ω ^)", ["happy", "blush"]);
+    addEmoticon("(╥﹏╥)", ["sad", "blush"]);
+    addEmoticon("(>_<)", ["angry", "sad"]);
+    addEmoticon("(•_•)", ["dull"]);
+    addEmoticon("(✿◠‿◠)", ["happy", "blush"]);
+    addEmoticon("(ʘ‿ʘ)", ["shocked", "happy"]); 
+    addEmoticon("(☞ﾟヮﾟ)☞", ["happy"]);
+    addEmoticon("(ノಠ益ಠ)ノ", ["angry", "brows"]);
+    addEmoticon("(づ｡◕‿‿◕｡)づ", ["happy", "blush"]);
+    addEmoticon("(⊙_☉)", ["shocked", "confused"]);
+    addEmoticon("(¬‿¬)", ["silly", "brows"]);
+    addEmoticon("(╯︵╰,)", ["sad"]);
+    addEmoticon("(✧ω✧)", ["happy", "cat"]);
+    addEmoticon("(ಥ﹏ಥ)", ["sad", "blush"]);
+    addEmoticon("(ʕ•ᴥ•ʔ)", ["happy"]);
+    addEmoticon("(ง'̀-'́)ง", ["angry"]);
 }
